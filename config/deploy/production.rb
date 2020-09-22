496c91c51d5e709f1e8b31b7143f3098d27b8f1f
shgtkshruch <shgtk.shruch@gmail.com>
2020-09-22 11:13:40 +0900

    Add rbenv settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "18.180.160.181", user: "centos", roles: %w{app db web}
+server "54.238.36.172", user: "centos", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
